760e7e4d946356fadb78ff9bf527a8d45683fe1e
Nadya-Semyonova <gerey05@gmail.com>
2026-09-08 12:01:07 +0200

    add:photos

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,14 +4,18 @@ const basePath = import.meta.env.BASE_URL;
 export const ARTIST_PHOTO = `${basePath}images/hero.jpg`;
 
 export const GALLERY_PHOTOS = [
-  { id: 1, url: `${basePath}images/1.jpg`, label: "Гламурный образ" },
-  { id: 2, url: `${basePath}images/2.jpg`, label: "Вечерний макияж" },
-  { id: 3, url: `${basePath}images/3.jpg`, label: "Свадебный образ" },
-  { id: 4, url: `${basePath}images/4.jpg`, label: "Fashion-съёмка" },
-  { id: 5, url: `${basePath}images/5.png`, label: "Праздничный образ" },
-  { id: 6, url: `${basePath}images/6.png`, label: "Профессиональная съёмка" },
-  { id: 7, url: `${basePath}images/7.jpg`, label: "Студийная съёмка" },
-  { id: 8, url: `${basePath}images/8.jpg`, label: "Креативный образ" },
+  { id: 1, url: `${basePath}images/1.jpg`, label: "Макияж на мероприятие" },
+  { id: 2, url: `${basePath}images/6.PNG`, label: "Вечерний макияж" },
+  { id: 3, url: `${basePath}images/8.jpg`, label: "Макияж touch up" },
+  { id: 4, url: `${basePath}images/7.jpg`, label: "Fashion-съёмка" },
+  { id: 5, url: `${basePath}images/12.jpg`, label: "Праздничный образ" },
+  { id: 6, url: `${basePath}images/2.jpg`, label: "Вечерний макияж" },
+  { id: 7, url: `${basePath}images/5.jpg`, label: "Свадебная укладка" },
+  { id: 8, url: `${basePath}images/4.jpg`, label: "Дневной макияж" },
+  { id: 8, url: `${basePath}images/3.jpg`, label: "Свадебная укладка" },
+  { id: 8, url: `${basePath}images/10.JPG`, label: "Вечерний макияж" },
+  { id: 8, url: `${basePath}images/13.jpg`, label: "Креативный образ" },
+  { id: 8, url: `${basePath}images/14.jpg`, label: "Дневной макияж" },
 ];
 // Навигация
 export const NAV_ITEMS = [
@@ -49,6 +53,7 @@ export const SERVICES = [
   {
     category: "Дополнительно",
     items: [
+      { name: "Обучение макияж для себя", price: "4 000 ₽" },
       { name: "Выезд к клиенту (в Калининграда)", price: "+ 1 500 ₽" },
       { name: "Выезд за город", price: "по договорённости" },
       { name: "Ранний выезд (до 9:00)", price: "+ 1 000 ₽" },
